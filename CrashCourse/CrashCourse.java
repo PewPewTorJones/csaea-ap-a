d25858c82b1466496566c1e9c25cc5c0bfa82a5b
PewPewTorJones <jones20288528@student.nhvweb.net>
2026-09-03 13:38:41 +0000

    Java3

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -65,8 +65,45 @@ public class CrashCourse {
 
 
     System.out.println( e && f); //short circuits after checking e
+    
+    // CASTING (converting)
+    int g = (int)5.5;
+    System.out.println(g);
+    double h = (double)5/6;
+    System.out.println(h);
+    
+    //STRINGS
 
+    String s1 = "Goodnight";
+    String s2 = " and ";
+    String s3 = "Goodbye";
+    String result = s1 + s2 + s3;
+    result += ", Cowboy.";
+    System.out.println("\n" + result + "\n");
+    
+    //Arrays
 
+    int[] arry1 = new int[10];
+    System.out.println(arry1);
+    System.out.println("length: " + arry1.length);
+
+    arry1[0] = 11;
+    arry1[1] = 2;
+
+    System.out.println("Remainder: " + arry1[0] % arry1[1]);
+
+    int[] arry2 = {34, 52, 3, 64, 32};
+    System.out.println("arry2: "+ arry2[3]);
+    
+    //2d Array (grid/table)
+    int[][] arryGrid1 = new int[4][3]; //[rows][columns]
+    
+    // 0 0 0
+    // 0 0 0
+    // 0 0 0
+    // 0 0 0
+    System.out.println("Rows: "+ arryGrid1.length);
+    System.out.println("Columns" + arry1Grid[0].length);
     }
 
 }
