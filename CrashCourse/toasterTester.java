6336ab1dbc415d6cad656d4bf3365142a8980cc2
PewPewTorJones <jones20288528@student.nhvweb.net>
2026-09-23 13:24:10 +0000

    javacompile

---
+++ CrashCourse/toasterTester.java
@@ -0,0 +1,13 @@
+public class toasterTester {
+    public static void main(String[] args) {
+
+        Toaster toaster1 = new Toaster(2, false, true);
+        Toaster toaster2 = new Toaster(4, true, false);
+
+        toaster1.unboxToaster();
+        toaster2.unboxToaster();
+
+        
+    }
+    
+}
